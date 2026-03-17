fbbb3455ffa9d00ae1b4cf232a5ad50adf9c5aa4
Natanael Heinrick <natanaelheinrick12@gmail.com>
2026-03-17 00:45:30 -0300

    feat: adicionando reconhecimento da letra A

--- letter_classifier.py
+++ letter_classifier.py
@@ -9,4 +9,17 @@ def classificar_letra(dedos, hand):
     anelar = hand.landmark[16]
     minimo = hand.landmark[20]
 
+    polegar = hand.landmark[4]
+
+    if dedos == [1, 0, 0, 0, 0]:
+
+        if (
+            indicador.y > hand.landmark[6].y
+            and medio.y > hand.landmark[10].y
+            and anelar.y > hand.landmark[14].y
+            and minimo.y > hand.landmark[18].y
+        ):
+
+            return "A"
+
     return ""
